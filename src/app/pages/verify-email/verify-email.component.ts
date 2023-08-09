4cd79ad20c6ff3c497c4b5490005ef10c3dd9640
saifudheen9745 <saifu.ofcl@gmail.com>
2023-08-10 01:10:40 +0530

    [update]: unsubscribed all the subscription on veeify-mail  on destroy

--- src/app/pages/verify-email/verify-email.component.ts
+++ src/app/pages/verify-email/verify-email.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { VerifyEmailService } from './verify-email.service';
 import { loginResponsePostConfirmation } from 'src/app/config/config.types';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
   styleUrls: ['./verify-email.component.css'],
 })
-export class VerifyEmailComponent implements OnInit {
+export class VerifyEmailComponent implements OnInit, OnDestroy {
+  confirmVerifyLinkSubscription:Subscription
+  activatedRouteSubscription:Subscription
+  deleteUserSubscription:Subscription
   alreadyVerified: boolean = false;
   constructor(
     private route: ActivatedRoute,
@@ -19,11 +23,11 @@ export class VerifyEmailComponent implements OnInit {
 
   ngOnInit(): void {
     // Access parameter using paramMap
-    this.route.paramMap.subscribe((params) => {
+    this.activatedRouteSubscription = this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       const token = params.get('token');
 
-      this.verifyEmailService
+      this.confirmVerifyLinkSubscription = this.verifyEmailService
         .confirmVerifyLink(id as string, token as string)
         .subscribe(
           (data: loginResponsePostConfirmation) => {
@@ -36,12 +40,18 @@ export class VerifyEmailComponent implements OnInit {
             if (error?.error?.error?.error?.msg === 'User already verified') {
               this.alreadyVerified = true;
             } else {
-              this.verifyEmailService
+              this.deleteUserSubscription = this.verifyEmailService
                 .deleteAUser(id as string)
                 .subscribe((data) => {});
             }
           }
         );
     });
+  }
+
+  ngOnDestroy(): void {
+      this.confirmVerifyLinkSubscription.unsubscribe()
+      this.deleteUserSubscription.unsubscribe()
+      this.activatedRouteSubscription.unsubscribe()
   }
 }
