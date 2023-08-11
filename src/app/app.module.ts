b9f69332bb6eb94f14075817bdf21bc39f09e4f3
saifudheen9745 <saifu.ofcl@gmail.com>
2023-08-11 17:21:26 +0530

    [update]: updated the app module with provider interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { localStorageSyncReducer, userDetailsReducer } from './shared/ngrx/ngrx.reducers';
+import { InterceptorService } from './shared/services/interceptor/interceptor.service';
 
 @NgModule({
   declarations: [AppComponent, ],
@@ -29,7 +30,9 @@ import { localStorageSyncReducer, userDetailsReducer } from './shared/ngrx/ngrx.
       }
     ),
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:InterceptorService,multi:true}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store'
 import { userDetailsSelector } from 'src/app/shared/ngrx/ngrx.reducers';
+import {AppState} from '../../shared/ngrx/ngrx.reducers'
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent implements OnInit{
-  constructor(private store:Store){}
+export class HomePageComponent implements OnInit {
+  constructor(private store: Store,private http:HttpClient) {}
   ngOnInit(): void {
-      
-      
+    
   }
+
+  
 }
