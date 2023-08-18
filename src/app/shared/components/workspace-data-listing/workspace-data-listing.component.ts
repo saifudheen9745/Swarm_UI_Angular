381dd84acf98276c5b307d9d5049c5a1a1f8aaa0
saifudheen9745 <saifu.ofcl@gmail.com>
2023-08-18 11:36:52 +0530

    [update]:updated workspace design and emial collection design

--- src/app/shared/components/workspace-data-listing/workspace-data-listing.component.ts
+++ src/app/shared/components/workspace-data-listing/workspace-data-listing.component.ts
@@ -4,10 +4,24 @@ import { project, workspace } from 'src/app/config/config.types';
 @Component({
   selector: 'app-workspace-data-listing',
   templateUrl: './workspace-data-listing.component.html',
-  styleUrls: ['./workspace-data-listing.component.css']
+  styleUrls: ['./workspace-data-listing.component.css'],
 })
 export class WorkspaceDataListingComponent {
-  @Input() heading:string
-  @Input() projects:project[]
-  @Input() workspace:workspace
+  @Input() heading: string;
+  @Input() projects: project[];
+  @Input() workspace: workspace;
+  projectSearch:project[]
+  searchKeyword: string = '';
+
+  constructor() {
+   
+    this.projectSearch = this.projects
+    
+  }
+
+  handleSearchKeyup() {
+    if(this.workspace === null){
+      this.projectSearch = this.projects.filter((project)=>project.name.includes(this.searchKeyword))
+    }
+  }
 }
